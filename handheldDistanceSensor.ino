769bb5af4996aec5b29e78754e1be5640b69b36c
Gibgib52 <Gibsonjhall@gmail.com>
2024-03-04 11:35:04 -0600

    problems

--- handheldDistanceSensor.ino
+++ handheldDistanceSensor.ino
@@ -8,7 +8,7 @@ using ace_sorting::shellSortKnuth;
 
 #define endl "\n"
 
-// why is lcd not global? why cant Menu.c see it??????????
+// why is lcd not global? why cant Menu.c or Ranger.h see it??????????
 const int rs = 12, en = 11, d4 = 9, d5 = 8, d6 = 7, d7 = 6;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 AlignedJoy stick(joyXpin, joyYpin);
