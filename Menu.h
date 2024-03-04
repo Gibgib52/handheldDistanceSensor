80d89ecd9dd8a23bd6e3595f2eeda7e3f3338f15
Gibgib52 <Gibsonjhall@gmail.com>
2024-03-04 11:28:20 -0600

    restructure into multiple files

---
+++ Menu.h
@@ -0,0 +1,17 @@
+const int UI_DELAY = 100; // ms delay for ui loops
+
+const int UP = 1;
+const int DOWN = -1;
+const int LEFT = -2;
+const int RIGHT = 2;
+const int BTN = 3;
+
+void flashTxt();
+void menuEnd();
+
+
+void menuSettings();
+void menuTest();
+void alarmLoop();
+void menuAlarm();
+void menuMain();
---
+++ Ranger.h
@@ -0,0 +1,68 @@
+#define trigPin 2 // orange
+#define echoPin 3 // blue
+
+const int MAX_SAMPLES = 100;
+int samples = 1;
+
+// returns distance in cm
+float getRange() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // pulse
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  float duration = pulseIn(echoPin, HIGH);
+  float distance = (duration*.0343)/2;
+  return distance;
+}
+
+float avgRange(int samples, bool debug = false) {
+  // get avg of samples
+  float dists[samples];
+  for(int i = 0; i < samples; i++){
+    if(debug){
+      lcd.setCursor(14,1);
+      lcd.print(i+1);
+    }
+    dists[i] = getRange();
+    delay(50);
+  }
+  
+  float total = 0;
+  for(int i = 0; i < samples; i++) {
+    total += dists[i];
+  }
+
+  float distance = total/samples;
+  return distance;
+}
+
+float medianRange(int samples, bool debug = false){
+  // gather samples
+  float dists[samples];
+  for(int i = 0; i < samples; i++){
+    if(debug){
+      lcd.setCursor(14,1);
+      lcd.print(i+1);
+    }
+    dists[i] = getRange();
+    delay(50);
+  }
+
+  // get median
+  int len = sizeof(dists);
+  shellSortKnuth(dists, len);
+
+  // if len is odd return middle, if len is even return avg of middle values
+  if(!len % 2){
+    return dists[len/2];
+  } else{
+    int hi = ceil(len/2);
+    int lo = floor(len/2);
+
+    return (hi + lo)/2;
+  }
+}
