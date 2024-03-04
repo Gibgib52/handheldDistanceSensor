16da9620dca4a24fdc8687bfe0fc0de3ad8f2151
Gibgib52 <Gibsonjhall@gmail.com>
2024-03-04 15:13:25 -0600

    Move to .cpp, not .ino

---
+++ handheldDistance.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+#include <LiquidCrystal.h>
+#include <AlignedJoy.h>
+#include <AceSorting.h>
+using ace_sorting::shellSortKnuth;
+
+#define trigPin 2 // orange
+#define echoPin 3 // blue
+
+#define trigButton 5
+#define joyXpin 0
+#define joyYpin 1
+
+
+// sketch here
+int main(){
+  // do not remove
+  init();
+  Serial.begin(9600);
+  Serial.println("serial started");
+
+  const int rs = 12, en = 11, d4 = 9, d5 = 8, d6 = 7, d7 = 6;
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  AlignedJoy stick(joyXpin, joyYpin);
+
+  lcd.begin(16,2);
+  lcd.print("working");
+
+
+
+
+  // do not remove
+  Serial.flush();
+}
+
