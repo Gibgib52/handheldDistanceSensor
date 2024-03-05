63fbd1ce6866d6df4e07ad72a44d715ee5acf87e
Gibgib52 <Gibsonjhall@gmail.com>
2024-03-05 11:02:54 -0600

    remove comment

--- GMenu.cpp
+++ GMenu.cpp
@@ -11,28 +11,6 @@ void setupM(){
   memRng = 5.2;
 }
 
-
-// // flashes text on lcd
-// // default 2 flashes, 100 ms interval
-// // preserves screen content(excluding what txt overwrites), doesnt lcd.clear()
-// void flashTxt(char* txt, int flashes = 2, int interval = 200, int x = 0, int y = 0){
-//   int len = strlen(txt);
-//   for(int i = 0; i < flashes; i++){
-//     // -- on
-//     lcd.setCursor(x,y);
-//     lcd.print(txt);
-//     delay(interval);
-
-//     // -- off
-//     lcd.setCursor(x,y);
-//     // print blanks len times
-//     for(int j = 0; j < len; j++){
-//       lcd.print(" ");
-//     }
-//     delay(interval);
-//   }
-// }
-
 // util funcs end
 
 // menu funcs start
