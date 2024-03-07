babdcd09dab3f1daa7a1c631c38e1576da09a7ed
Gibgib52 <Gibsonjhall@gmail.com>
2024-03-07 10:31:51 -0600

    create kana map

--- GDis.cpp
+++ GDis.cpp
@@ -3,7 +3,8 @@
 #include "GDis.hpp"
 #include "globals.hpp"
 #include <string.h>
-#include <Vector.h>
+// #include <Vector.h>
+#include <ArxContainer.h>
 
 // flashes text on lcd
 // default 2 flashes, 100 ms interval
@@ -33,7 +34,7 @@ void writeKana(char txt[]){
 
 
   // https://sentry.io/answers/split-string-in-cpp/
-  Vector<String> total;
+  std::vector<String> total;
   int i = 0;
   while(i < str.length()){
     i = str.indexOf(" ");// delim
@@ -55,77 +56,77 @@ void writeKana(char txt[]){
 }
 
 
-// will make this a map later
-namespace kana{
-  char a  = 0b10110001;
-  char i  = 0b10110010;
-  char u  = 0b10110011;
-  char e  = 0b10110100;
-  char o  = 0b10110101;
+std::map<String, char> kana {
+  {"a", 0b10110001},
+  {"i", 0b10110010},
+  {"u", 0b10110011},
+  {"e", 0b10110100},
+  {"o", 0b10110101},
+ 
+  {"xa", 0b10100111},
+  {"xi", 0b10101000},
+  {"xu", 0b10101001},
+  {"xe", 0b10101010},
+  {"xo", 0b10101011},
+ 
+  {"ka", 0b10110110},
+  {"ki", 0b10110111},
+  {"ku", 0b10111000},
+  {"ke", 0b10111001},
+  {"ko", 0b10111010},
+ 
+  {"sa", 0b10111011},
+  {"si", 0b10111100},
+  {"su", 0b10111101},
+  {"se", 0b10111110},
+  {"so", 0b10111111},
+ 
+  {"ta", 0b11000000},
+  {"ti", 0b11000001},
+  {"tu", 0b11000010},
+  {"te", 0b11000011},
+  {"to", 0b11000100},
+ 
+  {"xtu", 0b10101111},
+ 
+  {"na", 0b11000101},
+  {"ni", 0b11000110},
+  {"nu", 0b11000111},
+  {"ne", 0b11001000},
+  {"no", 0b11001001},
+ 
+  {"ha", 0b11001010},
+  {"hi", 0b11001011},
+  {"hu", 0b11001100},
+  {"he", 0b11001101},
+  {"ho", 0b11001110},
+ 
+  {"ma", 0b11001111},
+  {"mi", 0b11010000},
+  {"mu", 0b11010001},
+  {"me", 0b11010010},
+  {"mo", 0b11010011},
+ 
+  {"ya", 0b11010100},
+  {"yu", 0b11010101},
+  {"yo", 0b11010110},
+ 
+  {"xya", 0b10111100},
+  {"xyu", 0b10111101},
+  {"xyo", 0b10111110},
 
-  char xa  = 0b10100111;
-  char xi  = 0b10101000;
-  char xu  = 0b10101001;
-  char xe  = 0b10101010;
-  char xo  = 0b10101011;
+  {"ra", 0b11010111},
+  {"ri", 0b11011000},
+  {"ru", 0b11011001},
+  {"re", 0b11011010},
+  {"ro", 0b11011011},
 
-  char ka = 0b10110110;
-  char ki = 0b10110111;
-  char ku = 0b10111000;
-  char ke = 0b10111001;
-  char ko = 0b10111010;
-
-  char sa = 0b10111011;
-  char si = 0b10111100;
-  char su = 0b10111101;
-  char se = 0b10111110;
-  char so = 0b10111111;
-
-  char ta = 0b11000000;
-  char ti = 0b11000001;
-  char tu = 0b11000010;
-  char te = 0b11000011;
-  char to = 0b11000100;
-
-  char xtu = 0b10101111;
-
-  char na = 0b11000101;
-  char ni = 0b11000110;
-  char nu = 0b11000111;
-  char ne = 0b11001000;
-  char no = 0b11001001;
-
-  char ha = 0b11001010;
-  char hi = 0b11001011;
-  char hu = 0b11001100;
-  char he = 0b11001101;
-  char ho = 0b11001110;
-
-  char ma = 0b11001111;
-  char mi = 0b11010000;
-  char mu = 0b11010001;
-  char me = 0b11010010;
-  char mo = 0b11010011;
-
-  char ya = 0b11010100;
-  char yu = 0b11010101;
-  char yo = 0b11010110;
-
-  char xya = 0b10111100;
-  char xyu = 0b10111101;
-  char xyo = 0b10111110;
-
-  char ra = 0b11010111;
-  char ri = 0b11011000;
-  char ru = 0b11011001;
-  char re = 0b11011010;
-  char ro = 0b11011011;
-
-  char wo = 0b10100110;
-  char wa = 0b11011100;
-  char n  = 0b11011101;
+  {"wo", 0b10100110},
+  {"wa", 0b11011100},
+  {"nn", 0b11011101},
 
   // dakuten and handakuten
-  char ten = 0b11011110;
-  char han = 0b11011111;
-}
+  {"ten", 0b11011110},
+  {"han", 0b11011111}
+};
+
