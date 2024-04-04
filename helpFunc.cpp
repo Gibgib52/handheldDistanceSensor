f38554e22cd1a8cac9f2e5033f81d3c002bd0ad5
Gibgib52 <Gibsonjhall@gmail.com>
2024-04-04 14:47:32 -0500

    add some help msgs

--- helpFunc.cpp
+++ helpFunc.cpp
@@ -3,17 +3,40 @@
 #include "GDis.hpp"
 
 void h_ranging(){
-  String txt = "some text to type out on the lcd, very long so i can test the word wrap on typeText";
+  String txt = "the ultrasonic  "
+  "ranger has a max"
+  "range of 400 cm "
+  "and a min of 2cm"
+  ". for good dists"
+  "measure against "
+  "an object with  "
+  "1m^2 surf. area."
+  "and within 30deg"
+  "angle of obj    "
+  "careful of multi"
+  "path or uninten "
+  "ded echoes      ";
+
+
+
   typeText(txt);
   waitForInput();
 }
 
 void h_alarm(){
-  lcd.print("alarm help");
+  String txt = "pDist is the set"
+  "distance to test"
+  "for, if dist is "
+  "below pDist an  "
+  "alert will      "
+  "display on lcd  ";
+
+  typeText(txt);
   waitForInput();
 }
 
 void printHelp(int id){
+  waitForBtnUp();
   switch(id){
     case 0:
       h_ranging();
